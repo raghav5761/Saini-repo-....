a0d75d7f2a2d9c6a7ea876720bf4a3d75ed21a3a
raghav5761 <raghavgargtg@gmail.com>
2025-07-10 09:52:53 +0530

    Add files via upload

---
+++ vars.py
@@ -0,0 +1,13 @@
+#🇳‌🇮‌🇰‌🇭‌🇮‌🇱‌
+# Add your details here and then deploy by clicking on HEROKU Deploy button
+import os
+from os import environ
+
+API_ID = int(environ.get("API_ID", "25566754"))
+API_HASH = environ.get("API_HASH", "27609daf8ee847978797a359024e7607")
+BOT_TOKEN = environ.get("BOT_TOKEN", "8135851585:AAH8PI5DmoLQJ_7rqLwudB0PTI__7DcMygs")
+OWNER = int(environ.get("OWNER", "2073438175"))
+CREDIT = environ.get("CREDIT", "--ANJAN PERSON--")
+LOG_CHANNEL = "-1002886837153" # log group me bot ko admin narur banaye full rights ke sath
+#WEBHOOK = True  # Don't change this
+#PORT = int(os.environ.get("PORT", 8080))  # Default to 8000 if not set
